e1675b7d8cbba051d5840e9a228587ac65cc4351
Wille Faler <wille.faler@gmail.com>
2011-04-05 22:57:55 +0100

    updated to 0.3 release

--- project/build/BowlerQuickStartProject.scala
+++ project/build/BowlerQuickStartProject.scala
@@ -2,7 +2,7 @@ import sbt._
 
 
 class BowlerExamplesProject(info: ProjectInfo) extends DefaultWebProject(info){
-  val bowler = "org.bowlerframework" % "bowler-core_2.8.1" % "0.2.1"
+  val bowler = "org.bowlerframework" % "core_2.8.1" % "0.3"
 	
   val slf4jVersion = "1.6.0"
 	
