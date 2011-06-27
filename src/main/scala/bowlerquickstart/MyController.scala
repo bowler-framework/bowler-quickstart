96721bc382cc314e8d804dab6d3aa44645f484df
Wille Faler <wille.faler@gmail.com>
2011-06-27 21:40:30 +0100

    uses FunctionNameConventionRoutes

--- src/main/scala/bowlerquickstart/MyController.scala
+++ src/main/scala/bowlerquickstart/MyController.scala
@@ -1,6 +1,6 @@
 package bowlerquickstart
 
-import org.bowlerframework.controller.Controller
+import org.bowlerframework.controller.{Controller,FunctionNameConventionRoutes}
 import org.bowlerframework.model.{ ParameterMapper, Validations}
 import org.bowlerframework.view.{Renderable}
 
@@ -13,18 +13,12 @@ import org.bowlerframework.view.{Renderable}
  * - Renderable: allows you to render View Model objects.
  */
 
-class MyController extends Controller with ParameterMapper with Validations with Renderable {
+class MyController extends Controller with ParameterMapper with Validations with Renderable  with FunctionNameConventionRoutes {
 
 
   // simple, no args render, just renders the root view of /views/GET/index (or http 204 for JSON)
  // views are resolved by view-root ("/view" on the classpath by default) + HTTP Method + path,
 // in this case /views/GET/index. The ending of the template file (mustache, ssp, jade or scaml) will be auto-resolved in the order mentioned here.
 // for named params, the ":" of the Scalatra route definition will be replaced by "_" when looking up on the classpath.
-  get("/")((request, response) => {
-	render
-	
-	})
-
-
-
+  def `GET /` = render
 }
